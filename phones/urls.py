f64a1405c79f198095b442c655a2057c6347279e
diyora09 <dziyodullayeva602@gmail.com>
2026-03-14 19:59:24 +0430

    Initial commit: Django phone shop project

--- phones/urls.py
+++ phones/urls.py
@@ -5,8 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('phones/<int:phone_id>/', details, name='details'),
-    path('phones/', phones, name='phones'),
-
+    path('<int:phone_id>/', details, name='details'),
+    path('', phones, name='phones'),
 ]
 
